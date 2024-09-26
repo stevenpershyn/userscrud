5b121c4d94c5d1b5d7e752de5d47b3f5bdc9c2bc
Steven Pershyn <stevenpershyn@gmail.com>
2024-09-26 14:51:15 -0400

    First Commit

--- src/main/java/com/example/UserscrudApplication.java
+++ src/main/java/com/example/UserscrudApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import com.example.userscrud.entity.Post;
+import com.example.userscrud.entity.User;
+import com.fasterxml.jackson.databind.type.ArrayType;
 
 @SpringBootApplication
 public class UserscrudApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(UserscrudApplication.class, args);
+		SpringApplication.run(UserscrudApplication.class, args);
+		
 		
 		ConfigurableApplicationContext context = SpringApplication.run(UserscrudApplication.class, args); 
 		
